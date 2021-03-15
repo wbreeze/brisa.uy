afea5691bf794888fc6474e9124b42e281437d8a
Douglas Lovell <doug@wbreeze.com>
2021-03-15 06:44:21 -0300

    Move _drafts to docs folder

--- script/new_draft.sh
+++ script/new_draft.sh
@@ -6,8 +6,8 @@ fi
 ymd=`date +%Y-%m-%d`
 title=$1
 name=`echo ${title} | sed -E "s/[^[:alnum:]]+/-/g"`
-dest="./_drafts/${name}.md"
-template="./_drafts/template.md"
+dest="./docs/_drafts/${name}.md"
+template="./docs/_drafts/template.md"
 subst="s/title: title/title: ${title}/;s/date: .+/date: ${ymd}/"
 sed -E "${subst}" "${template}" >"${dest}"
 echo "Created ${dest}"
--- script/publish_draft.sh
+++ script/publish_draft.sh
@@ -10,14 +10,14 @@ fi
 src="$1"
 base_name=${src##*/}
 ymd=`date +%Y-%m-%d`
-dest="./_posts/${ymd}-${base_name}"
+dest="./docs/_posts/${ymd}-${base_name}"
 subst="s/date: .+/date: ${ymd}/"
 sed -E "${subst}" "${src}" >"${dest}"
 echo
 echo "Published ${dest}"
 head -n 12 ${dest}
 echo "Recent content of _posts:"
-ls -lt ./_posts | head -n 6
+ls -lt ./docs/_posts | head -n 6
 echo
 echo "Remove the draft?"
 echo "Press enter to leave it, or type \"y\" and then enter to delete it."
