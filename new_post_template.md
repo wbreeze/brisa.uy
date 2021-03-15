af170ca592a6e59e375d261ef9d991b127aca80d
Douglas Lovell <doug@wbreeze.com>
2021-03-15 09:05:05 -0300

    Move template for new posts

---
+++ new_post_template.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: title
+date: 2021-03-15
+categories:
+excerpt:
+link_note: "[internal link]({% link _posts/2019-07-30-post.md | relative_url %})"
+image_note: "![image]({% link /assets/images/image.jpg | relative_url %})"
+---
+
+
--- script/new_draft.sh
+++ script/new_draft.sh
@@ -7,7 +7,7 @@ ymd=`date +%Y-%m-%d`
 title=$1
 name=`echo ${title} | sed -E "s/[^[:alnum:]]+/-/g"`
 dest="./docs/_drafts/${name}.md"
-template="./docs/_drafts/template.md"
+template="./new_post_template.md"
 subst="s/title: title/title: ${title}/;s/date: .+/date: ${ymd}/"
 sed -E "${subst}" "${template}" >"${dest}"
 echo "Created ${dest}"
