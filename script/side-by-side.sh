ea93ef1d2fab9789a7c6234e44fb8e07de3484ce
Douglas Lovell <doug@wbreeze.com>
2022-08-08 19:41:51 -0500

    Fotos lado al lado o arriba y bajo

--- script/side-by-side.sh
+++ script/side-by-side.sh
@@ -1,5 +1,5 @@
 if [ -e "$1" -a -e "$2" ]; then
-  magick montage -geometry +1+2 "$1" "$2" ~/Desktop/side-by-side.jpg
+  magick montage -tile 2x -geometry +1+2 "$1" "$2" ~/Desktop/side-by-side.jpg
   ls -l ~/Desktop/side-by-side.jpg
 else
   echo 'Specify two source images'
