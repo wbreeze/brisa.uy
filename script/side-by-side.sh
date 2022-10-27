03826efbfc69468556449987a3229a70c09f342b
Douglas Lovell <doug@wbreeze.com>
2022-10-27 00:25:02 -0500

    Pone el foto lado al lado en la carpeta actuál

--- script/side-by-side.sh
+++ script/side-by-side.sh
@@ -1,6 +1,6 @@
 if [ -e "$1" -a -e "$2" ]; then
-  magick montage -tile 2x -geometry +1+2 "$1" "$2" ~/Desktop/side-by-side.jpg
-  ls -l ~/Desktop/side-by-side.jpg
+  magick montage -tile 2x -geometry +1+2 "$1" "$2" ./side-by-side.jpg
+  ls -l ./side-by-side.jpg
 else
   echo 'Specify two source images'
 fi
