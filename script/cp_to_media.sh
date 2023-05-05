b2d9408ad208013ad3002e32937aa4dbd78e2d4d
Douglas Lovell <doug@wbreeze.com>
2023-05-04 20:46:11 -0500

    Añade script para subir media

---
+++ script/cp_to_media.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ ! -f "$1" ]; then
+  echo 'specify a source file'
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo 'specify a destination key'
+  exit 2
+fi
+source=$1
+dest=$2
+aws s3 cp "${source}" "s3://photo-24899216db9780af64fc6451a6a4cec8/brisa/video/2023/${dest}"
