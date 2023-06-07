ea861847ef5baba9a1d1f7a581653ed36aafeda9
Douglas Lovell <doug@wbreeze.com>
2023-06-06 23:27:45 -0500

    Pone margen sobre side-by-side

--- script/side-by-side.sh
+++ script/side-by-side.sh
@@ -1,5 +1,5 @@
 if [ -e "$1" -a -e "$2" ]; then
-  magick montage -tile 2x -geometry +1+2 "$1" "$2" ./side-by-side.jpg
+  magick montage -tile 2x -shadow -geometry +6+6 "$1" "$2" ./side-by-side.jpg
   ls -l ./side-by-side.jpg
 else
   echo 'Specify two source images'
