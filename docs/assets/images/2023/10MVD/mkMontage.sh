1cc372bbca38ba9da7cf170dff8a1da82e46bf08
Douglas Lovell <doug@wbreeze.com>
2023-10-30 07:42:39 -0300

    Añada fotos para Montevideo

---
+++ docs/assets/images/2023/10MVD/mkMontage.sh
@@ -0,0 +1,8 @@
+magick montage -tile 5x -shadow -geometry +6+6 \
+IMG_20230917_130102_023.jpg \
+IMG_20230917_135303_464.jpg  \
+IMG_20231017_145603_669.jpg \
+IMG_20231010_104616_982.jpg \
+IMG_20231002_133059_719.jpg \
+Montage.jpg
+
