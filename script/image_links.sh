2890f6b71068f59be778a9a11a1d7906f8d7ac8f
Douglas Lovell <doug@wbreeze.com>
2023-12-31 08:10:04 -0500

    Specify image links relative to docs
    So I don't have to remove "docs" from the paths all of the time.

--- script/image_links.sh
+++ script/image_links.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+if [ ! -d docs/"$1" ]; then
+  echo 'specify an image directory relative to docs'
+  echo 'e.g. script/image_links.sh assets/images/...'
+  exit 1
+fi
+
+cd docs
+
 for fn in `ls -U -1 $1` ; do
   echo "![description]("
   echo "  {{ '$1/$fn' | relative_url }}"
   echo ")"
+  echo
 done
+
+cd ..
