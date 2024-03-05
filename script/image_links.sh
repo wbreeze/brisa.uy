9adc889d8dccd9519e084cbb4a0737874c3176eb
Douglas Lovell <doug@wbreeze.com>
2024-03-04 19:40:14 -0500

    Que sea más facil generar vinculos a los fotos

--- script/image_links.sh
+++ script/image_links.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-if [ ! -d docs/"$1" ]; then
-  echo 'specify an image directory relative to docs'
-  echo 'e.g. script/image_links.sh assets/images/...'
+if [ ! -d "$1" ]; then
+  echo 'specify an image directory'
+  echo 'e.g. script/image_links.sh docs/assets/images/...'
   exit 1
 fi
 
-cd docs
-
+rnn=`echo $1 | sed -E 's/\.?\/?docs\///'`
 for fn in `ls -U -1 $1` ; do
   echo "![description]("
-  echo "  {{ '$1/$fn' | relative_url }}"
+  echo "  {{ '$rnn/$fn' | relative_url }}"
   echo ")"
   echo
 done
