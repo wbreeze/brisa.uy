433a930a3d36d72561e895a870f3b5412cf27a12
Douglas Lovell <doug@wbreeze.com>
2024-04-12 18:26:37 -0400

    Que gen_fotos funciona sin PATH"

--- script/gen_fotos.sh
+++ script/gen_fotos.sh
@@ -1,2 +1,12 @@
-gallery.sh docs assets/galleries fotos
-c_transform.sh ../album/style/site.xsl docs fotos
+#!/bin/bash
+
+if [ ! -d "../album" ]; then
+  echo "As sibling to this project checkout GitHub repository,"
+  echo "https://github.com/wbreeze/album"
+  echo "cd .."
+  echo "git checkout git@github.com:wbreeze/album.git"
+  echo "cd brisa.uy"
+  exit 1
+fi
+../album/gallery.sh docs assets/galleries fotos
+../album/c_transform.sh ../album/style/site.xsl docs fotos
