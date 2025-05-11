a30eee175bbd7b36ee8a34461b4cb866f0634493
Douglas Lovell <doug@wbreeze.com>
2025-05-11 16:00:23 -0400

    Causa cp_to_media usar la llave 2025

--- script/cp_to_media.sh
+++ script/cp_to_media.sh
@@ -5,9 +5,9 @@ if [ ! -f "$1" ]; then
 fi
 if [ -z "$2" ]; then
   echo 'specify a destination {key} to append as'
-  echo 's3://bucket/brisa/video/2024/{key}'
+  echo 's3://bucket/brisa/video/2025/{key}'
   exit 2
 fi
 source=$1
 dest=$2
-aws s3 cp "${source}" "s3://photo-24899216db9780af64fc6451a6a4cec8/brisa/video/2024/${dest}"
+aws s3 cp "${source}" "s3://photo-24899216db9780af64fc6451a6a4cec8/brisa/video/2025/${dest}"
