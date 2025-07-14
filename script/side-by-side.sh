da3f0af7988a7f11b052e71b939b155a63d7c6e7
Douglas Lovell <doug@wbreeze.com>
2025-07-13 20:52:31 -0400

    Enable specify output file in side-by-side

--- script/side-by-side.sh
+++ script/side-by-side.sh
@@ -1,6 +1,11 @@
 if [ -e "$1" -a -e "$2" ]; then
-  magick montage -auto-orient -tile 2x -shadow -geometry +12+12 "$1" "$2" ./side-by-side.jpg
-  ls -l ./side-by-side.jpg
+  DEST="./side-by-side.jpg"
+  if [[ "$#" -gt 2 ]]; then
+    DEST="$3"
+  fi
+  magick montage -auto-orient -tile 2x -shadow -geometry +12+12 "$1" "$2" \
+    "${DEST}"
+  echo "Side by side sent to ${DEST}"
 else
   echo 'Specify two source images'
 fi
